f95e9fc067285f2c4ae6c92f74713ed079b6e06e
Student <example@example.com>
2018-03-15 11:40:44 -0700

    git testing

---
+++ LetterAdapter.java
@@ -0,0 +1,13 @@
+package com.epicodus.boggle;
+
+
+import android.content.Context;
+import android.widget.BaseAdapter;
+
+public class LetterAdapter extends BaseAdapter {
+    private Context mContext;
+    private String[] mLetters;
+
+    public LetterAdapter ()test
+
+}
